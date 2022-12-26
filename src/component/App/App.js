e728993d89e0f498d3badb35b5af7282ef0e4e71
bekturb <bekkgboy2@gmail.com>
2022-12-26 10:56:42 +0600

    added functions

--- src/component/App/App.js
+++ src/component/App/App.js
@@ -1,27 +1,63 @@
 import '../../styles/App.css';
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import ListItems from "../ListItems/ListItems";
+import {baseURL} from "../../Api/api";
+import axios from "axios";
 
 function App() {
 
     const [title, setTitle] = useState('');
     const [items, setItems] = useState([]);
+    const [completedItems, setCompletedItems] = useState([]);
+    const [unCompletedItems, setUnCompletedItems] = useState([]);
 
-    const handleChange = () => {
+    const handleChange =  async () => {
 
-        const newObj = {
-            id: items.length ? items[items.length - 1].id + 1 : 1,
-            title,
-            checked: false,
+        if (title.trim()){
+            const newObj = {
+                id: items.length ? items[items.length - 1].id + 1 : 1,
+                title,
+            }
+            try {
+                const response = await axios.post(`${baseURL}todo-items`, {title});
+                const data = response.data;
+                setItems([...items, data])
+            } catch (error) {
+                console.error(error);
+            }
+            setItems([...items, newObj])
         }
-        setItems([...items, newObj])
     }
-
 
     const deleteItem = (id) => {
         const deletedItem = items.filter(item => item.id !== id)
         setItems(deletedItem)
     }
+
+    const updateItem = (id, newItem) => {
+        const updatedItems = items.map(el => el.id === id ? {...el, title: newItem} : el);
+        setItems(updatedItems);
+    }
+
+    const getCompletedItems = (id) => {
+        const checkedItems = items.map(el => el.id === id ? {...el, checked: !el.checked} : el);
+        setItems(checkedItems);
+    }
+
+   useEffect( () => {
+
+       axios.get(`${baseURL}todo-items`)
+           .then(response => setItems(response.data))
+           .catch(error => {
+               setTitle(error.message );
+               console.error('There was an error!', error);
+           });
+
+            const completeItems = items.filter(el => el.checked === true)
+            setCompletedItems(completeItems)
+            const unCheckedItems = items.filter(el => el.checked === false);
+            setUnCompletedItems(unCheckedItems)
+    }, [items])
 
     return (
         <div className="App">
@@ -36,21 +72,19 @@ function App() {
                 <ul id="incomplete-tasks">
 
                     {
-                        items.map((el) => (
-                           <ListItems deleteItem={deleteItem} key={el.id} el={el}/>
+                        unCompletedItems.map((el) => (
+                           <ListItems getCompletedItems={getCompletedItems} updateItem={updateItem} deleteItem={deleteItem} key={el.id} el={el}/>
                         ))
                     }
                 </ul>
 
                 <h3>Completed</h3>
                 <ul id="completed-tasks">
-                    {/*<li>*/}
-                    {/*    <input type="checkbox" checked/>*/}
-                    {/*    <label>See the Doctor</label>*/}
-                    {/*    <input type="text"/>*/}
-                    {/*    <button className="edit">Edit</button>*/}
-                    {/*    <button className="delete">Delete</button>*/}
-                    {/*</li>*/}
+                    {
+                        completedItems.map(el => (
+                            <ListItems getCompletedItems={getCompletedItems} updateItem={updateItem} deleteItem={deleteItem} key={el.id} el={el}/>
+                        ))
+                    }
                 </ul>
             </div>
         </div>
--- src/component/ListItems/ListItems.js
+++ src/component/ListItems/ListItems.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const ListItems = ({el, deleteItem}) => {
+const ListItems = ({el, deleteItem, updateItem, getCompletedItems}) => {
+
+    const [newItem, setNewItem] = useState(el.title);
+    const [openedInput, setOpenedInput] = useState(false);
+
+    const addInputClass = () => {
+        setOpenedInput(true)
+    }
+
+    const removeInputClass = (id, newItem) => {
+        setOpenedInput(false)
+        updateItem(id, newItem)
+    }
+
+    const completedItem = (id) => {
+        getCompletedItems(id)
+    }
 
     return (
-        <li key={el.id}>
-            <input type="checkbox"/>
-            <label>{el.title}</label>
-            <input type="text"/>
-            <button  className="edit">Edit</button>
+        <li>
+            <input onChange={() => completedItem(el.id)} type="checkbox" checked={el.checked ? true : false} />
+            <label className={openedInput ? "form__label-edit" : ""}>{el.title}</label>
+            <input className={openedInput ? "form__input-edit" : ""} onChange={(e) => setNewItem(e.target.value)} type="text" defaultValue={el.title}/>
+            <button onClick={() => {openedInput ? removeInputClass(el.id, newItem) : addInputClass()}}  className="edit">{openedInput ?  "Close" : "Edit"}</button>
             <button onClick={() => {deleteItem(el.id)}} className="delete">Delete</button>
         </li>
     );
